576e830c18dc585993f5e8f1ce059e70c726e979
abdulrehman8210345 <abdulrehman8210345@gmail.com>
2024-05-30 01:37:10 +0500

    vercel.json file changes

--- backend/database/dbconnection.js
+++ backend/database/dbconnection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const dbConnection = async () => {
-    mongoose.connect(process.env.MONGO_URI,{dbName:"JobSeekingApplication"}).then(()=>{
+    mongoose.connect(process.env.MONGO_URI,{dbName:"Job_Seeking_Application"}).then(()=>{
         console.log("database connected")
     }).catch((err)=>{
         console.log(`database connection failed due to ${err}`)
